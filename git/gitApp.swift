7b5e8557de2cc00f884dc5cc81fd57a3cfd41787
yponion <yp071704@naver.com>
2024-05-13 23:57:25 +0900

    first commit

---
+++ git/gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitApp.swift
+//  git
+//
+//  Created by yp on 2024/05/13.
+//
+
+import SwiftUI
+
+@main
+struct gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
